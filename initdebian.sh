5b7562c6cafd17034162ba66a449393447558991
Layne Wedderburn <layne@wedderburn.co.za>
2024-03-21 12:51:27 +0000

    Build as user, install as root
    Clone and build tools as default user and install tools as root. This
    way the repos are not left in the root user's $HOME dir which would be
    inconvenient.
    
    WIP: Build as user, install as root

--- initdebian.sh
+++ initdebian.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 [[ $UID != 0 ]] && echo "must run as root" && exit 1
+[[ -z $(grep ":1000:" /etc/passwd) ]] && echo "must have default user with id=1000 configured" && exit 1
 
 echo "Begin initialisation"
 
@@ -39,10 +40,33 @@ apt install -y \
 
 # Install window manager and tools
 
-mkdir -p $HOME/.config/suckless
-cd $HOME/.config/suckless
+default_user=$(id -nu 1000);
+default_user_home=$(grep :1000: /etc/passwd | cut -d: -f6)
 
-echo "dwm st dmenu sxiv xwallpaper" | tr ' ' '\n' | \
-    xargs -I {} bash -c "git clone https://github.com/laynew/{}.git; cd {}; make clean install"
+mkdir -p $default_user_home/.config/suckless
+cd $default_user_home/.config/suckless
 
+chown -R 1000:1000 $default_user_home/.config/
+
+su -p $default_user <<'EOF'
+for app in "dwm" "st" "dmenu" "sxiv"; do
+    git clone "https://github.com/laynew/$app"
+    pushd $app
+    make
+    popd
+done
+
+git clone https://github.com/laynew/xwallpaper
+pushd xwallpaper
+./autogen.sh
+./configure
+make
+popd
+EOF
+
+for app in "dwm" "st" "dmenu" "sxiv" "xwallpaper"; do
+    pushd $app
+    make install
+    popd
+done
 
